5e80e86f28b1c19232755b98fdb2fea07fb22099
TillBrede <51406030+TillBrede@users.noreply.github.com>
2019-09-02 22:37:41 +0200

    applied cs fixer style (#5)

--- ZaehlerUeberlauf/module.php
+++ ZaehlerUeberlauf/module.php
@@ -1,55 +1,52 @@
-<?
+<?php
 
-	class ZaehlerUeberlauf extends IPSModule
-	{
-		
-		public function Create()
-		{
-			//Never delete this line!
-			parent::Create();
-			
-			$this->RegisterPropertyInteger("SourceVariable", 0);
-			$this->RegisterPropertyInteger("MaximumValue", 999999);
-			
-			$this->RegisterVariableFloat("Counter", "Counter", "", 1);
-		}
-	
-		public function ApplyChanges()
-		{
-			
-			//Never delete this line!
-			parent::ApplyChanges();
-			
-			//Create our trigger
-			if(IPS_VariableExists($this->ReadPropertyInteger("SourceVariable"))) {
-				$this->RegisterMessage($this->ReadPropertyInteger("SourceVariable"), VM_UPDATE);
-				//Deleting events used in legacy
-				$eid = @IPS_GetObjectIDByIdent("SourceTrigger", $this->InstanceID);
-				if($eid) {
-					IPS_DeleteEvent($this->GetIDForIdent("SourceTrigger"));
-				}
-			}
-			
-		}
-	
-		private function Update(int $OldValue, int $Value)
-		{
-			if (($Value - $OldValue) < 0) {
-				$diff = $this->ReadPropertyInteger("MaximumValue") + 1 - $OldValue + $Value;
-			} else {
-				$diff = $Value - $OldValue;
-			}
-			
-			//update value
-			SetValue($this->GetIDForIdent("Counter"), GetValue($this->GetIDForIdent("Counter")) + $diff);
-		
-		}
-	
-		public function MessageSink($TimeStamp, $SenderID, $Message, $Data)
-		{	
-			//VM_UPDATE
-			$this->Update($Data[2], $Data[0]);
-		}
-	}
+declare(strict_types=1);
 
-?>
+    class ZaehlerUeberlauf extends IPSModule
+    {
+        public function Create()
+        {
+            //Never delete this line!
+            parent::Create();
+
+            $this->RegisterPropertyInteger('SourceVariable', 0);
+            $this->RegisterPropertyInteger('MaximumValue', 999999);
+
+            $this->RegisterVariableFloat('Counter', 'Counter', '', 1);
+        }
+
+        public function ApplyChanges()
+        {
+
+            //Never delete this line!
+            parent::ApplyChanges();
+
+            //Create our trigger
+            if (IPS_VariableExists($this->ReadPropertyInteger('SourceVariable'))) {
+                $this->RegisterMessage($this->ReadPropertyInteger('SourceVariable'), VM_UPDATE);
+                //Deleting events used in legacy
+                $eid = @IPS_GetObjectIDByIdent('SourceTrigger', $this->InstanceID);
+                if ($eid) {
+                    IPS_DeleteEvent($this->GetIDForIdent('SourceTrigger'));
+                }
+            }
+        }
+
+        private function Update(int $OldValue, int $Value)
+        {
+            if (($Value - $OldValue) < 0) {
+                $diff = $this->ReadPropertyInteger('MaximumValue') + 1 - $OldValue + $Value;
+            } else {
+                $diff = $Value - $OldValue;
+            }
+
+            //update value
+            SetValue($this->GetIDForIdent('Counter'), GetValue($this->GetIDForIdent('Counter')) + $diff);
+        }
+
+        public function MessageSink($TimeStamp, $SenderID, $Message, $Data)
+        {
+            //VM_UPDATE
+            $this->Update($Data[2], $Data[0]);
+        }
+    }
